7ff04c7396b71de79600702dc9a6ec9d3a7bb313
Pierre Lasalmonie <p.lasalmonie@gmail.com>
2015-05-12 20:53:06 +0200

    Add Test Folder for Unit Test and some Test Class
    Creation of the test folder.
    Creation of FormateurTest.java and FormationTest.java
    They are class for Unit Testing of their corresponding class

---
+++ test/model/FormateurTest.java
@@ -0,0 +1,206 @@
+
+package model;
+
+import java.util.Date;
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lasalmonie Pierre
+ */
+public class FormateurTest {
+
+    private Formateur leFormateur;
+    private Module leModule;
+    private HashMap<Integer, Seance> lesSeances;
+
+    public FormateurTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        lesSeances = new HashMap<Integer, Seance>();
+        leFormateur = new Formateur(1, "Dujardin", "Jean", "JD", "0102030405", "Jean.Dujardin@u-pec.fr", lesSeances);
+        leModule = new Module(1, "Base de données", "BDD", "Black", 10, lesSeances);
+        Date dateSeanceUn = new Date();
+        Seance seanceUn = new Seance(1, 1, dateSeanceUn, leModule, leFormateur);
+        Date dateSeanceDeux = new Date();
+        Seance seanceDeux = new Seance(1, 2, dateSeanceDeux, leModule, leFormateur);
+        lesSeances.put(1, seanceUn);
+        lesSeances.put(2, seanceDeux);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class Formateur.
+     */
+    @Test
+    public void testGetId() {
+        int expResult = 1;
+        int result = leFormateur.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class Formateur.
+     */
+    @Test
+    public void testSetId() {
+        int expResult = 2;
+        leFormateur.setId(expResult);
+        int result = leFormateur.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getNom method, of class Formateur.
+     */
+    @Test
+    public void testGetNom() {
+        String expResult = "Dujardin";
+        String result = leFormateur.getNom();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setNom method, of class Formateur.
+     */
+    @Test
+    public void testSetNom() {
+        String expResult = "LaFontaine";
+        leFormateur.setNom(expResult);
+        String result = leFormateur.getNom();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getPrenom method, of class Formateur.
+     */
+    @Test
+    public void testGetPrenom() {
+        String expResult = "Jean";
+        String result = leFormateur.getPrenom();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setPrenom method, of class Formateur.
+     */
+    @Test
+    public void testSetPrenom() {
+        String expResult = "Pedro";
+        leFormateur.setPrenom(expResult);
+        String result = leFormateur.getPrenom();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getInitiales method, of class Formateur.
+     */
+    @Test
+    public void testGetInitiales() {
+        String expResult = "JD";
+        String result = leFormateur.getInitiales();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setInitiales method, of class Formateur.
+     */
+    @Test
+    public void testSetInitiales() {
+        String expResult = "PL";
+        leFormateur.setInitiales(expResult);
+        String result = leFormateur.getInitiales();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getTelephone method, of class Formateur.
+     */
+    @Test
+    public void testGetTelephone() {
+        String expResult = "0102030405";
+        String result = leFormateur.getTelephone();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setTelephone method, of class Formateur.
+     */
+    @Test
+    public void testSetTelephone() {
+        String expResult = "0101010101";
+        leFormateur.setTelephone(expResult);
+        String result = leFormateur.getTelephone();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getEmail method, of class Formateur.
+     */
+    @Test
+    public void testGetEmail() {
+        String expResult = "Jean.Dujardin@u-pec.fr";
+        String result = leFormateur.getEmail();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setEmail method, of class Formateur.
+     */
+    @Test
+    public void testSetEmail() {
+        String expResult = "JDujardin@u-pec.fr";
+        leFormateur.setEmail(expResult);
+        String result = leFormateur.getEmail();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getListeSceances method, of class Formateur.
+     */
+    @Test
+    public void testGetListeSeances() {
+        HashMap<Integer, Seance> result = leFormateur.getListeSceances();
+        HashMap<Integer, Seance> expResult = lesSeances;
+        assertSame(expResult, result);
+    }
+
+    /**
+     * Test of setListeSceances method, of class Formateur.
+     */
+    @Test
+    public void testSetListeSceances() {
+        HashMap<Integer, Seance> lesNouvllesSeances = new HashMap<Integer, Seance>();
+        leModule = new Module(1, "Base de données", "BDD", "Black", 10, lesSeances);
+        Date dateSeanceTrois = new Date();
+        Seance seanceTrois = new Seance(1, 1, dateSeanceTrois, leModule, leFormateur);
+        Date dateSeanceQuatre = new Date();
+        Seance seanceQuatre = new Seance(1, 2, dateSeanceQuatre, leModule, leFormateur);
+        lesNouvllesSeances.put(1, seanceTrois);
+        lesNouvllesSeances.put(2, seanceQuatre);
+        leFormateur.setListeSceances(lesNouvllesSeances);
+        HashMap<Integer, Seance> result = leFormateur.getListeSceances();
+        HashMap<Integer, Seance> expResult = lesNouvllesSeances;
+        assertSame(expResult, result);
+    }
+
+}
---
+++ test/model/FormationTest.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.HashMap;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Xiiz
+ */
+public class FormationTest {
+
+    private Formation laFormation;
+    private Module moduleFra;
+    private HashMap<Integer, Module> listeModules;
+    public FormationTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        HashMap<Integer, Seance> lesSeances = new HashMap<Integer, Seance>();
+        Module moduleBdd = new Module(1, "Base de données", "BDD", "Black", 10, lesSeances);
+        moduleFra = new Module(1, "Français", "FRA", "Blue", 5, lesSeances);
+        listeModules = new HashMap<Integer, Module>();
+        laFormation = new Formation(1,"L3 2/3 A", 40, listeModules);
+        laFormation.addModule(1,moduleBdd);
+        laFormation.addModule(2,moduleFra);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getId method, of class Formation.
+     */
+    @Test
+    public void testGetId() {
+        int expResult = 1;
+        int result = laFormation.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setId method, of class Formation.
+     */
+    @Test
+    public void testSetId() {
+        int expResult = 3;
+        laFormation.setId(expResult);
+        int result = laFormation.getId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getNom method, of class Formation.
+     */
+    @Test
+    public void testGetNom() {
+        String expResult = "L3 2/3 A";
+        String result = laFormation.getNom();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setNom method, of class Formation.
+     */
+    @Test
+    public void testSetNom() {
+        String expResult = "Master 1";
+        laFormation.setNom(expResult);
+        String result = laFormation.getNom();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getDureeSceance method, of class Formation.
+     */
+    @Test
+    public void testGetDureeSceance() {
+        int expResult = 40;
+        int result = laFormation.getDureeSceance();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setDureeSceance method, of class Formation.
+     */
+    @Test
+    public void testSetDureeSceance() {
+        int expResult = 45;
+        laFormation.setDureeSceance(expResult);
+        int result = laFormation.getDureeSceance();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getListeModules method, of class Formation.
+     */
+    @Test
+    public void testGetListeModules() {
+        HashMap<Integer, Module> expResult = listeModules;
+        HashMap<Integer, Module> result = laFormation.getListeModules();
+        assertSame(expResult, result);
+    }
+
+    /**
+     * Test of getModule method, of class Formation.
+     */
+    @Test
+    public void testGetModule() {
+        Module expResult = moduleFra;
+        Module result = laFormation.getModule(2);
+        assertSame(expResult, result);
+    }
+
+    /**
+     * Test of setListeModules method, of class Formation.
+     */
+    @Test
+    public void testSetListeModules() {
+        HashMap<Integer, Module> listeNouveauxModules = new HashMap<Integer, Module>();
+        HashMap<Integer, Seance> lesNouvellesSeances = new HashMap<Integer, Seance>();
+        Module moduleEng = new Module(1, "Anglais", "ENG", "Yellow", 5, lesNouvellesSeances);
+        Module moduleMarketing = new Module(1, "Marketing", "Mark", "Orange", 10, lesNouvellesSeances);
+        listeNouveauxModules.put(Integer.SIZE, moduleEng);
+        listeNouveauxModules.put(Integer.SIZE, moduleMarketing);        
+        laFormation.setListeModules(listeNouveauxModules);
+        HashMap<Integer, Module> expResult = listeNouveauxModules;
+        HashMap<Integer, Module> result = laFormation.getListeModules();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of addModule method, of class Formation.
+     */
+    @Test
+    public void testAddModule() {
+        HashMap<Integer, Seance> lesSeances = new HashMap<Integer, Seance>();
+        Module moduleEng = new Module(1, "Anglais", "ENG", "Yellow", 5, lesSeances);
+        laFormation.addModule(3, moduleEng);
+        Module expResult = moduleEng;
+        Module result = laFormation.getModule(3);
+        assertEquals(expResult, result);
+    }
+
+}
